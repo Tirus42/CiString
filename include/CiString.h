86347eb949401d04ecc0b186df9ec379e0d016ef
Tirus <tirus@mail.com>
2023-11-01 13:24:57 +0100

    Init

---
+++ include/CiString.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+/**
+ * Case insensitive string implementation
+ * based on std::string.
+ *
+ * Just overrides the <, >, == and != operator.
+*/
+class CiString : public std::string {
+  public:
+    CiString();
+    CiString(const char* str);
+    CiString(const std::string& str);
+    CiString(const CiString& str);
+
+    bool operator<(const std::string& other) const;
+    bool operator>(const std::string& other) const;
+
+    bool operator==(const std::string& other) const;
+    bool operator!=(const std::string& other) const;
+};
